acf32447af85f8f453783d53b661ab8b99abe3e5
PranauR <ajaypranauer10@gmail.com>
2022-03-03 19:02:07 +0530

    #3: Updated source code as per MCCI Standards

--- src/BubbleSort-Ten-Numbers.c
+++ src/BubbleSort-Ten-Numbers.c
@@ -1,28 +1,66 @@
-#include <stdio.h>
-int main()
-{
-    int arr[10], n=10, i, j, swap;                   //declaring integer
-    /*printf("Enter Number of Elements:\n");         //Getting array size from user
-    scanf("%d", &n); */                                
-    printf("Now Enter 10 Random Intergers:\n", n);   //Getting n number of integer input from user
-    for (i=0; i<n; ++i)                              //storing all inputs on their corresponding array index
-    scanf("%d", &arr[i]);
+/*
 
-    for (i=0; i<n-1; ++i)                            //Loop command for sorting the inputs
+Module: BubbleSort-Ten-Numbers.c
+
+Function:
+    Bubble sorting given 10 numbers from user and print in ascending order.
+
+Copyright notice:
+    This file copyright (C) 2022 by
+    
+    MCCI Corporation
+    3520 Krums Corners Road
+    Ithaca, NY 14850
+    
+    An unpublished work. All rights reserved.
+
+    This file is proprietary information, and may not be disclosed or
+    copied without the prior permission of MCCI Corporation.
+
+Author:
+    Pranau R, MCCI Corporation   March 2022
+
+*/
+
+#include <stdio.h>
+
+/****************************************************************************\
+|
+|   Code.
+|
+\****************************************************************************/
+
+int main()
     {
-        for (j=0; j<n-i-1; ++j)
+    int array[10], num, index, next, swap; 
+    num = 10;
+
+    // Getting n number of integer input from user
+    printf("Enter 10 Random Intergers:\n", num);
+
+    // storing all inputs on their corresponding array index
+    for (index = 0; index < num; ++index)
+        scanf("%d", &array[index]);
+
+    // Loop command for sorting the inputs
+    for (index = 0 ; index < num - 1; ++index)
         {
-            if (arr[j]>arr[j+1])                     //comparing the value of arr[j] and arr[j+1], we can also use '<' operator in the place of '>' operator for soting in descending order.
+        for (next = 0; next < num -index -1; ++next)
             {
-                swap = arr[j];                       //swaping the values if the condition is true a temporary variable 'swap'
-                arr[j] = arr[j+1];
-                arr[j+1] = swap;
+            if (array[next] > array[next + 1])             // comparing the value of arr[next] and arr[next+1]
+                {
+                swap = array[next];                        // swaping the values if the condition is true a temporary variable 'swap'
+                array[next] = array[next + 1];
+                array[next + 1] = swap;
+                }
             }
         }
-    }
 
-printf("Sorted list in ascending order:\n");        //printing all our sorted numbers one by one
-for (i=0; i<n; ++i)
-printf("%d\n", arr[i]);
-return 0;
-}
+    // printing all our sorted numbers one by one
+    printf("Sorted list in ascending order:\n");
+
+    for (index = 0; index < num; ++index)
+        printf("%d\n", array[index]);
+
+    return 0;
+    }
